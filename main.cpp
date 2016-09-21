7b6651addbbe0b2c8ff89ddf5f8bfd01ad7e0285
JasmineIslamNY <jasmine@tek-genie.com>
2016-09-21 11:21:18 -0400

    20160921_1121

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FSDPrep_CPP
+//
+//  Created by Jasmine Islam on 9/21/16.
+//  Copyright © 2016 Jasmine Islam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
