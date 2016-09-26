33527935768df19f3f849d84e0bbb3712cae3043
JasmineIslamNY <jasmine@tek-genie.com>
2016-09-25 20:53:35 -0400

    20160925_2053 (Ch 4 Problem 17)

---
+++ fourSeventeen.cpp
@@ -0,0 +1,30 @@
+/** Chapter 4 Problem 17
+*/
+
+#include <iostream>
+
+/*Main method
+ *Dynamically create pieces of storage of the following types, using new: int, long, an array of 100 chars, an array of 100 floats. Print the addresses of these and then free the storage using delete.
+ *param: none
+ *return: none
+ *throw: none
+ */
+
+int main() {
+    using namespace std;
+    
+    int* p_int = new int;
+    long* p_long = new long;
+    char* p_chars = new char[100];
+    float* p_floats = new float[100];
+    
+    cout << "p_int == " << p_int << endl;
+    cout << "p_long == " << p_long << endl;
+    cout << "p_chars == " << static_cast<void*>(p_chars) << endl;
+    cout << "p_floats == " << p_floats << endl;
+    
+    delete p_int;
+    delete p_long;
+    delete [] p_chars;
+    delete [] p_floats;
+}
